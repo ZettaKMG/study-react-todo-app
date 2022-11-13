8cec5848a0df5e7c2188aac4dc4bed910465c66a
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-13 16:29:40 +0900

    app에서 만든 onToggle 함수를 TodoListItem에서 호출가능하도록 TodoList를 거쳐 TodoListItem에게 전달

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,15 +3,23 @@
 // TodoListItem에서 삭제 함수 호출하기
 // props로 받아온 onRemove 함수를 TodoListItem에 그대로 전달.
 
+// TodoListItem에서 토글 함수 호출하기
+// app에서 만든 onToggle 함수를 TodoListItem에서 호출가능하도록 TodoList를 거쳐 TodoListItem에게 전달.
+
 import React from "react";
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
 
-const TodoList = ({todos, onRemove}) => {
+const TodoList = ({todos, onRemove, onToggle}) => {
     return (
         <div className="TodoList">
             {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+                <TodoListItem 
+                    todo={todo} 
+                    key={todo.id} 
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
             ))}
         </div>
     );
