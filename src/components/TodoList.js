203d3983d43b16e05cceace44c2c84bf854bf6bd
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-14 17:01:10 +0900

    TodoList 컴포넌트 최적화하기
    리스트 관련 컴포넌트 최적화 => 리스트 내부 컴포넌트, 리스트 자체 컴포넌트 양쪽 다 최적화 해줄 것.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,6 +6,8 @@
 // TodoListItem에서 토글 함수 호출하기
 // app에서 만든 onToggle 함수를 TodoListItem에서 호출가능하도록 TodoList를 거쳐 TodoListItem에게 전달.
 
+// TodoList 컴포넌트 최적화하기
+// 리스트 관련 컴포넌트 최적화 => 리스트 내부 컴포넌트, 리스트 자체 컴포넌트 양쪽 다 최적화 해줄 것.
 import React from "react";
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
@@ -25,4 +27,4 @@ const TodoList = ({todos, onRemove, onToggle}) => {
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
