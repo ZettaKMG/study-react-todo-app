cce9c0dac06fd53f0d2b52799b00db48579711bf
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-14 18:01:13 +0900

    TodoListItem 수정

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -17,7 +17,34 @@
 
 // React.memo를 사용하여 컴포넌트 성능 최적화
 // todo, onRemove, onToggle이 바뀌지 않으면 리렌더링 하지 않게 조치.
+// import React from 'react';
+// import {
+//     MdCheckBoxOutlineBlank,
+//     MdCheckBox,
+//     MdRemoveCircleOutline,
+// } from 'react-icons/md';
+// import cn from 'classnames';
+// import './TodoListItem.scss';
 
+// const TodoListItem = ({todo, onRemove, onToggle}) => {
+//     const {id, text, checked} = todo;
+
+//     return (
+//         <div className="TodoListItem">
+//             <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
+//                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+//                 <div className="text">{text}</div>
+//             </div>
+//             <div className="remove" onClick={() => onRemove(id)}>
+//                 <MdRemoveCircleOutline />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default React.memo(TodoListItem);
+
+// TodoListItem 수정
 import React from 'react';
 import {
     MdCheckBoxOutlineBlank,
